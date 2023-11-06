af8827d269c5f8e2cbcb03cd4747a8ab5ec92fcd
CaIon <1808837298@qq.com>
2023-11-06 21:45:36 +0800

    change name to miaoko api

--- web/src/components/SiderBar.js
+++ web/src/components/SiderBar.js
@@ -179,7 +179,7 @@ const HeaderBar = () => {
                         }}
                         header={{
                             logo: <img src={logo} alt='logo' style={{marginRight: '0.75em'}}/>,
-                            text: 'NekoAPI'
+                            text: 'MiaoKoAPI'
                         }}
                         // footer={{
                         //   text: '© 2021 NekoAPI',
