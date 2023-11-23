3e89112b1cde496408b09205f692cf3d64b8f562
luxl <726857632@qq.com>
2023-11-23 18:18:03 +0800

    绘图列表更新

--- controller/midjourney.go
+++ controller/midjourney.go
@@ -148,7 +148,16 @@ func GetAllMidjourney(c *gin.Context) {
 	if p < 0 {
 		p = 0
 	}
-	logs := model.GetAllTasks(p*common.ItemsPerPage, common.ItemsPerPage)
+
+	// 解析其他查询参数
+	queryParams := model.TaskQueryParams{
+		ChannelID:      c.Query("channel_id"),
+		MjID:           c.Query("mj_id"),
+		StartTimestamp: c.Query("start_timestamp"),
+		EndTimestamp:   c.Query("end_timestamp"),
+	}
+
+	logs := model.GetAllTasks(p*common.ItemsPerPage, common.ItemsPerPage, queryParams)
 	if logs == nil {
 		logs = make([]*model.Midjourney, 0)
 	}
@@ -164,9 +173,17 @@ func GetUserMidjourney(c *gin.Context) {
 	if p < 0 {
 		p = 0
 	}
+
 	userId := c.GetInt("id")
 	log.Printf("userId = %d \n", userId)
-	logs := model.GetAllUserTask(userId, p*common.ItemsPerPage, common.ItemsPerPage)
+
+	queryParams := model.TaskQueryParams{
+		MjID:           c.Query("mj_id"),
+		StartTimestamp: c.Query("start_timestamp"),
+		EndTimestamp:   c.Query("end_timestamp"),
+	}
+
+	logs := model.GetAllUserTask(userId, p*common.ItemsPerPage, common.ItemsPerPage, queryParams)
 	if logs == nil {
 		logs = make([]*model.Midjourney, 0)
 	}
