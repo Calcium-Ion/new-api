e41fcd563e1bf4fe4333ad93c16ca91322ead13b
CaIon <1808837298@qq.com>
2024-01-13 01:32:23 +0800

    chore: 数据看板限制用户只能查询跨度一个月的数据

--- controller/usedata.go
+++ controller/usedata.go
@@ -31,6 +31,14 @@ func GetUserQuotaDates(c *gin.Context) {
 	userId := c.GetInt("id")
 	startTimestamp, _ := strconv.ParseInt(c.Query("start_timestamp"), 10, 64)
 	endTimestamp, _ := strconv.ParseInt(c.Query("end_timestamp"), 10, 64)
+	// 判断时间跨度是否超过 1 个月
+	if endTimestamp-startTimestamp > 2592000 {
+		c.JSON(http.StatusOK, gin.H{
+			"success": false,
+			"message": "时间跨度不能超过 1 个月",
+		})
+		return
+	}
 	dates, err := model.GetQuotaDataByUserId(userId, startTimestamp, endTimestamp)
 	if err != nil {
 		c.JSON(http.StatusOK, gin.H{
