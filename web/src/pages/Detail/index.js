e8188902c266a2721576b8744c44f52e68a88c7e
CaIon <1808837298@qq.com>
2024-01-25 14:57:46 +0800

    feat: 数据看板加入每列总计

--- web/src/pages/Detail/index.js
+++ web/src/pages/Detail/index.js
@@ -90,9 +90,16 @@ const Detail = (props) => {
                     // sort by value
                     array.sort((a, b) => b.value - a.value);
                     // add $
+                    let sum = 0;
                     for (let i = 0; i < array.length; i++) {
+                        sum += parseFloat(array[i].value);
                         array[i].value = renderQuotaNumberWithDigit(parseFloat(array[i].value), 4);
                     }
+                    // add to first
+                    array.unshift({
+                        key: '总计',
+                        value: renderQuotaNumberWithDigit(sum, 4)
+                    });
                     return array;
                 }
             }
