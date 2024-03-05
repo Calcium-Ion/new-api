92c1ed7f1dfc7e2f31379f8b9e9191de5abf185e
QuentinHsu <xuquentinyang@gmail.com>
2024-03-05 12:18:30 +0800

    perf: prompt when the name of the custom model input already exists

--- web/src/pages/Channel/EditChannel.js
+++ web/src/pages/Channel/EditChannel.js
@@ -241,7 +241,7 @@ const EditChannel = (props) => {
 
     const addCustomModel = () => {
         if (customModel.trim() === '') return;
-        if (inputs.models.includes(customModel)) return;
+        if (inputs.models.includes(customModel)) return showError("该模型已存在！");
         let localModels = [...inputs.models];
         localModels.push(customModel);
         let localModelOptions = [];
