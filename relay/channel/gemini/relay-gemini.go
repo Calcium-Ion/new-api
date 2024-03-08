8fe8340b6e1f1f5001174b7573b93254b1486b71
CaIon <1808837298@qq.com>
2024-03-08 21:38:51 +0800

    fix: fix gemini channel test

--- relay/channel/gemini/relay-gemini.go
+++ relay/channel/gemini/relay-gemini.go
@@ -140,8 +140,8 @@ func responseGeminiChat2OpenAI(response *GeminiChatResponse) *dto.OpenAITextResp
 			},
 			FinishReason: relaycommon.StopFinishReason,
 		}
-		content, _ = json.Marshal(candidate.Content.Parts[0].Text)
 		if len(candidate.Content.Parts) > 0 {
+			content, _ = json.Marshal(candidate.Content.Parts[0].Text)
 			choice.Message.Content = content
 		}
 		fullTextResponse.Choices = append(fullTextResponse.Choices, choice)
