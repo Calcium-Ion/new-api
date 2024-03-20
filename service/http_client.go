e0bcbf3beb0762eca3aee3d07d183f02516c928f
coder_tq <58659074+coder-tq@users.noreply.github.com>
2024-03-20 19:46:18 +0800

    [feature] 支持 307 重定向

--- service/http_client.go
+++ service/http_client.go
@@ -11,10 +11,19 @@ var impatientHTTPClient *http.Client
 
 func init() {
 	if common.RelayTimeout == 0 {
-		httpClient = &http.Client{}
+		httpClient = &http.Client{
+			CheckRedirect: func(req *http.Request, via []*http.Request) error {
+				// 自定义重定向策略，例如限制重定向次数或修改请求头
+				return nil // 允许重定向
+			},
+		}
 	} else {
 		httpClient = &http.Client{
 			Timeout: time.Duration(common.RelayTimeout) * time.Second,
+			CheckRedirect: func(req *http.Request, via []*http.Request) error {
+				// 自定义重定向策略，例如限制重定向次数或修改请求头
+				return nil // 允许重定向
+			},
 		}
 	}
 
