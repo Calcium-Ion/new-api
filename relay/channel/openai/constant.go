6c059d5bf21d61afd8712df9a3df290f5ee7b611
Maple Gao <esanisa@gmail.com>
2024-05-14 09:46:39 +0800

    ✨ add: Support GPT4o

--- relay/channel/openai/constant.go
+++ relay/channel/openai/constant.go
@@ -1,6 +1,7 @@
 package openai
 
 var ModelList = []string{
+	"gpt-4o",
 	"gpt-3.5-turbo", "gpt-3.5-turbo-0301", "gpt-3.5-turbo-0613", "gpt-3.5-turbo-1106", "gpt-3.5-turbo-0125",
 	"gpt-3.5-turbo-16k", "gpt-3.5-turbo-16k-0613",
 	"gpt-3.5-turbo-instruct",
