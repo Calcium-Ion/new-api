b82582cb427c3931c4b8e9981d0639e019bf4db4
Xiangyuan Liu <xiangyuan.liu@ui.com>
2024-06-13 18:28:47 +0800

    revert: hide web embed

--- main.go
+++ main.go
@@ -20,10 +20,10 @@ import (
 	_ "net/http/pprof"
 )
 
-// /go:embed web/dist
+//go:embed web/dist
 var buildFS embed.FS
 
-// /go:embed web/dist/index.html
+//go:embed web/dist/index.html
 var indexPage []byte
 
 func main() {
