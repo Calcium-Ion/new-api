fc6ae6bf34e3c70613903f598fa0dcb16db8be44
CalciumIon <1808837298@qq.com>
2024-06-27 17:17:23 +0800

    feat: 支持设置流模式超时时间

--- common/go-channel.go
+++ common/go-channel.go
@@ -3,6 +3,7 @@ package common
 import (
 	"fmt"
 	"runtime/debug"
+	"time"
 )
 
 func SafeGoroutine(f func()) {
@@ -44,4 +45,22 @@ func SafeSendString(ch chan string, value string) (closed bool) {
 
 	// If the code reaches here, then the channel was not closed.
 	return false
+}
+
+// SafeSendStringTimeout send, return true, else return false
+func SafeSendStringTimeout(ch chan string, value string, timeout int) (closed bool) {
+	defer func() {
+		// Recover from panic if one occured. A panic would mean the channel was closed.
+		if recover() != nil {
+			closed = false
+		}
+	}()
+
+	// This will panic if the channel is closed.
+	select {
+	case ch <- value:
+		return true
+	case <-time.After(time.Duration(timeout) * time.Second):
+		return false
+	}
 }
--- constant/system-setting.go
+++ constant/system-setting.go
@@ -1,8 +1,12 @@
 package constant
 
+import "one-api/common"
+
 var ServerAddress = "http://localhost:3000"
 var WorkerUrl = ""
 var WorkerValidKey = ""
+
+var StreamingTimeout = common.GetOrDefault("STREAMING_TIMEOUT", 30)
 
 func EnableWorker() bool {
 	return WorkerUrl != ""
