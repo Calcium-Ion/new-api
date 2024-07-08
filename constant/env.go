a984daa50307e814b6133ccfabd7fdbc2e3c8f0e
CalciumIon <1808837298@qq.com>
2024-07-08 21:41:52 +0800

    feat: update FORCE_STREAM_OPTION default value

--- constant/env.go
+++ constant/env.go
@@ -8,4 +8,4 @@ var StreamingTimeout = common.GetEnvOrDefault("STREAMING_TIMEOUT", 30)
 var DifyDebug = common.GetEnvOrDefaultBool("DIFY_DEBUG", true)
 
 // ForceStreamOption 覆盖请求参数，强制返回usage信息
-var ForceStreamOption = common.GetEnvOrDefaultBool("FORCE_STREAM_OPTION", true)
+var ForceStreamOption = common.GetEnvOrDefaultBool("FORCE_STREAM_OPTION", false)
