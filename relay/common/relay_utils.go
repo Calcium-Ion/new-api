a518463493bba0bfe456061395741cf877bf601f
arelice <141432992+arelice@users.noreply.github.com>
2024-07-17 22:50:13 +0800

    Update relay_utils.go

--- relay/common/relay_utils.go
+++ relay/common/relay_utils.go
@@ -17,32 +17,38 @@ import (
 
 var StopFinishReason = "stop"
 
-func RelayErrorHandler(resp *http.Response) (OpenAIErrorWithStatusCode *dto.OpenAIErrorWithStatusCode) {
-	OpenAIErrorWithStatusCode = &dto.OpenAIErrorWithStatusCode{
-		StatusCode: resp.StatusCode,
-		Error: dto.OpenAIError{
-			Message: fmt.Sprintf("bad response status code %d", resp.StatusCode),
-			Type:    "upstream_error",
-			Code:    "bad_response_status_code",
-			Param:   strconv.Itoa(resp.StatusCode),
-		},
-	}
-	responseBody, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return
-	}
-	err = resp.Body.Close()
-	if err != nil {
-		return
-	}
-	var textResponse dto.TextResponseWithError
-	err = json.Unmarshal(responseBody, &textResponse)
-	if err != nil {
-		OpenAIErrorWithStatusCode.Error.Message = fmt.Sprintf("error unmarshalling response body: %s", responseBody)
-		return
-	}
-	OpenAIErrorWithStatusCode.Error = textResponse.Error
-	return
+func RelayErrorHandler(resp *http.Response) (ErrorWithStatusCode *relaymodel.ErrorWithStatusCode) {
+    if resp == nil {
+        return &relaymodel.ErrorWithStatusCode{
+            StatusCode: 500,
+            Error: relaymodel.Error{
+                Message: "请稍后再试试，如长时间不可用，请通知管理员q2411728460 进行处理",
+                Type:    "model_unavailable",
+                Code:    "model_unavailable",
+            },
+        }
+    }
+
+    ErrorWithStatusCode = &relaymodel.ErrorWithStatusCode{
+        StatusCode: resp.StatusCode,
+        Error: relaymodel.Error{
+            Message: "请稍后再试试，如长时间不可用，请通知管理员q2411728460 进行处理",
+            Type:    "model_unavailable",
+            Code:    "model_unavailable",
+        },
+    }
+
+    // 读取上游响应体（可选，如果需要记录上游错误信息）
+    _, err := io.ReadAll(resp.Body)
+    if err != nil {
+        // 不记录错误
+    }
+    err = resp.Body.Close()
+    if err != nil {
+        // 不记录错误
+    }
+
+    return
 }
 
 func GetFullRequestURL(baseURL string, requestURL string, channelType int) string {
