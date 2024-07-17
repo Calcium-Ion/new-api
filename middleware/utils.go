4c45d803786a2b8519a9b1ecbefd5f938fdb4d91
arelice <141432992+arelice@users.noreply.github.com>
2024-07-18 01:10:03 +0800

    Update utils.go

--- middleware/utils.go
+++ middleware/utils.go
@@ -9,7 +9,7 @@ func abortWithOpenAiMessage(c *gin.Context, statusCode int, message string) {
 	c.JSON(statusCode, gin.H{
 		"error": gin.H{
 			"message": common.MessageWithRequestId(message, c.GetString(common.RequestIdKey)),
-			"type":    "new_api_error",
+			"type":    "rjl_api_error",
 		},
 	})
 	c.Abort()
@@ -19,7 +19,7 @@ func abortWithOpenAiMessage(c *gin.Context, statusCode int, message string) {
 func abortWithMidjourneyMessage(c *gin.Context, statusCode int, code int, description string) {
 	c.JSON(statusCode, gin.H{
 		"description": description,
-		"type":        "new_api_error",
+		"type":        "rjl_api_error",
 		"code":        code,
 	})
 	c.Abort()
