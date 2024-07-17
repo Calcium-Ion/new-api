125c74a98df49ec0649daf38274b3eda1f208ac8
arelice <141432992+arelice@users.noreply.github.com>
2024-07-17 23:43:17 +0800

    Update relay_utils.go

--- relay/common/relay_utils.go
+++ relay/common/relay_utils.go
@@ -9,34 +9,31 @@ import (
 	"io"
 	"net/http"
 	"one-api/common"
+	"one-api/dto"
 	"strings"
 )
 
 var StopFinishReason = "stop"
 
-// 定义一个新的错误类型
-type CustomError struct {
-	StatusCode int
-	Message    string
-	Type       string
-	Code       string
-}
-
-func RelayErrorHandler(resp *http.Response) *CustomError {
+func RelayErrorHandler(resp *http.Response) *dto.OpenAIErrorWithStatusCode {
 	if resp == nil {
-		return &CustomError{
+		return &dto.OpenAIErrorWithStatusCode{
 			StatusCode: 500,
-			Message:    "请稍后再试试，如长时间不可用，请通知管理员q2411728460 进行处理",
-			Type:       "model_unavailable",
-			Code:       "model_unavailable",
+			Error: dto.OpenAIError{
+				Message: "请稍后再试试，如长时间不可用，请通知管理员q2411728460 进行处理",
+				Type:    "model_unavailable",
+				Code:    "model_unavailable",
+			},
 		}
 	}
 
-	customError := &CustomError{
+	openAIError := &dto.OpenAIErrorWithStatusCode{
 		StatusCode: resp.StatusCode,
-		Message:    "请稍后再试试，如长时间不可用，请通知管理员q2411728460 进行处理",
-		Type:       "model_unavailable",
-		Code:       "model_unavailable",
+		Error: dto.OpenAIError{
+			Message: "请稍后再试试，如长时间不可用，请通知管理员q2411728460 进行处理",
+			Type:    "model_unavailable",
+			Code:    "model_unavailable",
+		},
 	}
 
 	// 读取上游响应体（可选，如果需要记录上游错误信息）
@@ -49,7 +46,7 @@ func RelayErrorHandler(resp *http.Response) *CustomError {
 		// 不记录错误
 	}
 
-	return customError
+	return openAIError
 }
 
 func GetFullRequestURL(baseURL string, requestURL string, channelType int) string {
