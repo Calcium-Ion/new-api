3d522758cf45b26d07bf7a3e3ec708b05635d4cc
arelice <141432992+arelice@users.noreply.github.com>
2024-07-17 23:33:40 +0800

    Update relay_utils.go

--- relay/common/relay_utils.go
+++ relay/common/relay_utils.go
@@ -1,7 +1,6 @@
 package common
 
 import (
-	"one-api/relaymodel"
 	"fmt"
 	"github.com/gin-gonic/gin"
 	_ "image/gif"
@@ -10,44 +9,47 @@ import (
 	"io"
 	"net/http"
 	"one-api/common"
-	
 	"strings"
 )
 
 var StopFinishReason = "stop"
 
-func RelayErrorHandler(resp *http.Response) (ErrorWithStatusCode *relaymodel.ErrorWithStatusCode) {
-    if resp == nil {
-        return &relaymodel.ErrorWithStatusCode{
-            StatusCode: 500,
-            Error: relaymodel.Error{
-                Message: "请稍后再试试，如长时间不可用，请通知管理员q2411728460 进行处理",
-                Type:    "model_unavailable",
-                Code:    "model_unavailable",
-            },
-        }
-    }
+// 定义一个新的错误类型
+type CustomError struct {
+	StatusCode int
+	Message    string
+	Type       string
+	Code       string
+}
 
-    ErrorWithStatusCode = &relaymodel.ErrorWithStatusCode{
-        StatusCode: resp.StatusCode,
-        Error: relaymodel.Error{
-            Message: "请稍后再试试，如长时间不可用，请通知管理员q2411728460 进行处理",
-            Type:    "model_unavailable",
-            Code:    "model_unavailable",
-        },
-    }
+func RelayErrorHandler(resp *http.Response) *CustomError {
+	if resp == nil {
+		return &CustomError{
+			StatusCode: 500,
+			Message:    "请稍后再试试，如长时间不可用，请通知管理员q2411728460 进行处理",
+			Type:       "model_unavailable",
+			Code:       "model_unavailable",
+		}
+	}
 
-    // 读取上游响应体（可选，如果需要记录上游错误信息）
-    _, err := io.ReadAll(resp.Body)
-    if err != nil {
-        // 不记录错误
-    }
-    err = resp.Body.Close()
-    if err != nil {
-        // 不记录错误
-    }
+	customError := &CustomError{
+		StatusCode: resp.StatusCode,
+		Message:    "请稍后再试试，如长时间不可用，请通知管理员q2411728460 进行处理",
+		Type:       "model_unavailable",
+		Code:       "model_unavailable",
+	}
 
-    return
+	// 读取上游响应体（可选，如果需要记录上游错误信息）
+	_, err := io.ReadAll(resp.Body)
+	if err != nil {
+		// 不记录错误
+	}
+	err = resp.Body.Close()
+	if err != nil {
+		// 不记录错误
+	}
+
+	return customError
 }
 
 func GetFullRequestURL(baseURL string, requestURL string, channelType int) string {
