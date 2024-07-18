b5ce2307365e4eccc41c75d4f9c44ea2010a758c
arelice <141432992+arelice@users.noreply.github.com>
2024-07-18 11:09:14 +0800

    Update constants.go

--- common/constants.go
+++ common/constants.go
@@ -11,7 +11,7 @@ import (
 
 var StartTime = time.Now().Unix() // unit: second
 var Version = "v0.0.0"            // this hard coding will be replaced automatically when building, no need to manually change
-var SystemName = "New API"
+var SystemName = "RJL API"
 var Footer = ""
 var Logo = ""
 var TopUpLink = ""
