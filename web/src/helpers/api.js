ce815a98d0255042925bbbf8852a668dd5eeb4e2
CalciumIon <1808837298@qq.com>
2024-07-19 13:39:05 +0800

    fix: 修复nginx缓存导致串用户问题

--- web/src/helpers/api.js
+++ web/src/helpers/api.js
@@ -1,10 +1,13 @@
-import { showError } from './utils';
+import { getUserIdFromLocalStorage, showError } from './utils';
 import axios from 'axios';
 
 export const API = axios.create({
   baseURL: import.meta.env.VITE_REACT_APP_SERVER_URL
     ? import.meta.env.VITE_REACT_APP_SERVER_URL
     : '',
+  headers: {
+    'New-API-User': getUserIdFromLocalStorage()
+  }
 });
 
 API.interceptors.response.use(
--- web/src/helpers/utils.js
+++ web/src/helpers/utils.js
@@ -33,6 +33,13 @@ export function getLogo() {
   return logo;
 }
 
+export function getUserIdFromLocalStorage() {
+  let user = localStorage.getItem('user');
+  if (!user) return -1;
+  user = JSON.parse(user);
+  return user.id;
+}
+
 export function getFooterHTML() {
   return localStorage.getItem('footer_html');
 }
