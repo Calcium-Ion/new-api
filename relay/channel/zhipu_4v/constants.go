97c18d0c7fcc275ca077d5ed82f7368ce3ae0844
Nanami <shizurin233@gmail.com>
2024-08-31 10:20:22 +0800

    feat: support more zhipu models

--- relay/channel/zhipu_4v/constants.go
+++ relay/channel/zhipu_4v/constants.go
@@ -1,7 +1,7 @@
 package zhipu_4v
 
 var ModelList = []string{
-	"glm-4", "glm-4v", "glm-3-turbo", "glm-4-alltools",
+	"glm-4", "glm-4v", "glm-3-turbo", "glm-4-alltools", "glm-4-plus", "glm-4-0520", "glm-4-air", "glm-4-airx", "glm-4-long", "glm-4-flash", "glm-4v-plus",
 }
 
 var ChannelName = "zhipu_4v"
