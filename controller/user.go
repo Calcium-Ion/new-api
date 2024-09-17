9032b5cfbf2be69d2bdb2f6bf547fc28981165ce
CalciumIon <1808837298@qq.com>
2024-09-17 23:07:16 +0800

    fix: 初始令牌

--- controller/user.go
+++ controller/user.go
@@ -187,7 +187,7 @@ func Register(c *gin.Context) {
 		})
 		return
 	}
-	
+
 	// 获取插入后的用户ID
 	var insertedUser model.User
 	if err := model.DB.Where("username = ?", cleanUser.Username).First(&insertedUser).Error; err != nil {
@@ -199,23 +199,15 @@ func Register(c *gin.Context) {
 	}
 	// 生成默认令牌
 	if constant.GenerateDefaultToken {
-		var insertedUser model.User
-		if err := model.DB.Where("username = ?", cleanUser.Username).First(&insertedUser).Error; err != nil {
-			c.JSON(http.StatusOK, gin.H{
-				"success": false,
-				"message": "用户注册失败或用户ID获取失败",
-			})
-			return
-		}
 		// 生成默认令牌
 		token := model.Token{
 			UserId:             insertedUser.Id, // 使用插入后的用户ID
-			Name:               cleanUser.Username + "的初始令牌",  
+			Name:               cleanUser.Username + "的初始令牌",
 			Key:                common.GenerateKey(),
 			CreatedTime:        common.GetTimestamp(),
 			AccessedTime:       common.GetTimestamp(),
-			ExpiredTime:        -1,  // 永不过期
-			RemainQuota:        500000,  // 示例额度
+			ExpiredTime:        -1,     // 永不过期
+			RemainQuota:        500000, // 示例额度
 			UnlimitedQuota:     true,
 			ModelLimitsEnabled: false,
 		}
