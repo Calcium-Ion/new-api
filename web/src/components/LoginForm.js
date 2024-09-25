b139588aa1ceb7b44d90745f53ad27c5f11c71a3
GuoRuqiang <61670021+guoruqiang@users.noreply.github.com>
2024-09-25 06:38:17 +0000

    更新LoginForm 提示

--- web/src/components/LoginForm.js
+++ web/src/components/LoginForm.js
@@ -170,7 +170,7 @@ const LoginForm = () => {
                   <Form.Input
                     field={'username'}
                     label={'用户名'}
-                    placeholder='用户名'
+                    placeholder='请输入用户名或邮箱'
                     name='username'
                     onChange={(value) => handleChange('username', value)}
                   />
