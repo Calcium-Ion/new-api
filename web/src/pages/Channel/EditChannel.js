cf8b30edfad1b3bdfcaa236918f4781f2325a105
CalciumIon <1808837298@qq.com>
2024-12-09 21:26:17 +0800

    fix: edit channel weight and priority

--- web/src/pages/Channel/EditChannel.js
+++ web/src/pages/Channel/EditChannel.js
@@ -964,7 +964,12 @@ const EditChannel = (props) => {
             name="priority"
             placeholder={'渠道优先级'}
             onChange={(value) => {
-              handleInputChange('priority', parseInt(value));
+              const number = parseInt(value);
+              if (isNaN(number)) {
+                handleInputChange('priority', value);
+              } else {
+                handleInputChange('priority', number);
+              }
             }}
             value={inputs.priority}
             autoComplete="new-password"
@@ -979,7 +984,12 @@ const EditChannel = (props) => {
             name="weight"
             placeholder={'渠道权重'}
             onChange={(value) => {
-              handleInputChange('weight', parseInt(value));
+              const number = parseInt(value);
+              if (isNaN(number)) {
+                handleInputChange('weight', value);
+              } else {
+                handleInputChange('weight', number);
+              }
             }}
             value={inputs.weight}
             autoComplete="new-password"
