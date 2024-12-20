f3a71197457c9345fc0c8d1bd035231ac8a4e6f5
HynoR <20227709+HynoR@users.noreply.github.com>
2024-12-20 19:17:56 +0800

    chore: 更新gemini模型

--- relay/channel/gemini/constant.go
+++ relay/channel/gemini/constant.go
@@ -8,7 +8,7 @@ var ModelList = []string{
 	// stable version
 	"gemini-1.5-pro", "gemini-1.5-flash", "gemini-1.5-flash-8b",
 	// latest version
-	"gemini-1.5-pro-latest", "gemini-1.5-flash-latest", "gemini-1.5-flash-8b-latest",
+	"gemini-1.5-pro-latest", "gemini-1.5-flash-latest",
 	// legacy version
 	"gemini-1.5-pro-exp-0827", "gemini-1.5-flash-exp-0827",
 	// exp
