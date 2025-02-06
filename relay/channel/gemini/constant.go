efdd6fb65721f344b8a17ea764d008a1a6e2046f
HynoR <20227709+HynoR@users.noreply.github.com>
2025-02-06 13:32:19 +0800

    chore: sync gemini aistudio model

--- relay/channel/gemini/constant.go
+++ relay/channel/gemini/constant.go
@@ -3,17 +3,19 @@ package gemini
 var ModelList = []string{
 	// stable version
 	"gemini-1.5-pro", "gemini-1.5-flash", "gemini-1.5-flash-8b",
+	"gemini-2.0-flash",
 	// latest version
 	"gemini-1.5-pro-latest", "gemini-1.5-flash-latest",
-	// legacy version
-	"gemini-1.5-pro-exp-0827", "gemini-1.5-flash-exp-0827",
-	// exp
-	"gemini-exp-1114", "gemini-exp-1121", "gemini-exp-1206",
+	// preview version
+	"gemini-2.0-flash-lite-preview",
+	// gemini exp
+	"gemini-exp-1206",
 	// flash exp
 	"gemini-2.0-flash-exp",
+	// pro exp
+	"gemini-2.0-pro-exp",
 	// thinking exp
 	"gemini-2.0-flash-thinking-exp",
-	"gemini-2.0-flash-thinking-exp-1219",
 }
 
 var ChannelName = "google gemini"
