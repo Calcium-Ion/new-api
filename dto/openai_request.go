af00f7b3115334aa03fc19141fd210e8ae8cbceb
Elsa <zeyugao@outlook.com>
2025-02-24 10:52:55 +0800

    Pass extra_body to the backend

--- dto/openai_request.go
+++ dto/openai_request.go
@@ -47,6 +47,7 @@ type GeneralOpenAIRequest struct {
 	Dimensions          int             `json:"dimensions,omitempty"`
 	Modalities          any             `json:"modalities,omitempty"`
 	Audio               any             `json:"audio,omitempty"`
+	ExtraBody           any             `json:"extra_body,omitempty"`
 }
 
 type OpenAITools struct {
